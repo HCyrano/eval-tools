0ea7fb4d41ecb3a9e1b4cea3bbd2b9ae196b249f
bruno.causse <hcyrano@free.fr>
2025-11-05 12:04:37 +0100

    Initial Commit

---
+++ Evaluation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Evaluation
+//
+//  Created by Causse Bruno on 05/11/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
